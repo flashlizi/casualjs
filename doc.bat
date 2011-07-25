1ffb238238808a31c9dad9ef78bb5b0a34bfb20d
flashlizi <flashlizi@gmail.com>
2011-07-25 15:04:45 +0800

    doc modification

--- doc.bat
+++ doc.bat
@@ -1,1 +1,1 @@
-java -jar D:/dev/html5/jsdoc_toolkit-2.4.0/jsrun.jar D:/dev/html5/jsdoc_toolkit-2.4.0/app/run.js D:/experiment/casualjs/src/ -r=3 -a -d=D:/experiment/casualjs/doc -t=D:/experiment/casualjs/doc_template --exclude="casual.js" --exclude="EventManager.js" --exclude="Astar.js"
+cmd /k java -jar E:/dev/js/jsdoc_toolkit-2.4.0/jsrun.jar E:/dev/js/jsdoc_toolkit-2.4.0/app/run.js src/ -r=3 -a -d=doc/ -t=doc_template/ --exclude="casual.js" --exclude="EventManager.js" --exclude="Astar.js"
